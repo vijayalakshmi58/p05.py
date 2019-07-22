ab59f8e6e8b49f77790fd2d1301beb870f3cbbfa
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-07-22 14:17:02 +0530

    Update and rename README.md to p05.py

---
+++ p05.py
@@ -0,0 +1,8 @@
+# p05.py
+v1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+a2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+b1=str(input())
+for g1 in range(20):
+  if(b1==v1[d1]):
+    print(a2[g1])
+    break
